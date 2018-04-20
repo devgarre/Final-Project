85e10c75651f69d0df909f9ad0eac37e3dc797ac
KNOWN-UNIVERSE\devgarre <devgarre@uat.edu>
2018-04-20 15:10:05 -0700

    Program almost complete Need to add pointers

---
+++ Final Project/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+void timer(float min);
+string name(string input);
+void talkBack(string userName);
+string rerun(string repeat);
---
+++ Final Project/Source.cpp
@@ -0,0 +1,36 @@
+// Source.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+#include <chrono>
+#include <windows.h>
+
+using namespace std;
+
+void timer(float min)
+{
+	Sleep(min * 60000);
+}
+
+string name(string input)
+{
+	string userInput = "";
+	cout << input;
+	cin >> userInput;
+	return userInput;
+}
+
+void talkBack(string userName) 
+{
+	cout << "It's time to get up and walk around, " << userName << "." << endl;
+	cout << "There's really no point though as we are all going to meet our end on an unexpected day that even the supercomputer, Deep Thought, cannot predict." << endl;
+}
+
+string rerun(string repeat) 
+{
+	cout << "Would you like to start another timer?(Y/N): ";
+	cin >> repeat;
+	return repeat;
+}
